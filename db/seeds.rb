903d79882c2c0b2b642ee6c39c780dfc0e655b1d
jungah sohn <aliciasohn815@gmail.com>
2021-05-06 03:39:17 +0200

    movie_rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Cleaning our db..."
+
+# delete join table first
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Grabbing new movies..."
+
+url = URI.open("http://tmdb.lewagon.com/movie/top_rated").read
+
+# parse it
+# movies = JSON.parse(url)['results']
+
+movies = JSON.parse(url)['results']
+
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['backdrop_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts "Created #{Movie.count} movie(s)"
