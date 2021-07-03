e1ba58e7cbbc93bf11af0a18fa3c39f4a8babf46
jungah sohn <aliciasohn815@gmail.com>
2021-07-03 22:39:22 +0200

    rails watch

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,8 @@
 class List < ApplicationRecord
-  has_one_attached :photo
-  
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_many :reviews
+  has_one_attached :photo
 
-  validates :name, presence: true 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
 end
